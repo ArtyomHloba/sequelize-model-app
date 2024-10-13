506bb9346454145d11b7f8ff39c644b166d47ccd
ArtyomHloba <artemgloba2@gmail.com>
2024-10-13 16:00:01 +0300

    Add createUser controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,15 @@
-module.exports.createUser = async (req, res, next) => {};
+const { User } = require('./../models');
+
+module.exports.createUser = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const createdUser = await User.create(body);
+    res.status(201).send(createdUser.get());
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports.getUsers = async (req, res, next) => {
   res.status(501).send('GET /api/users Not Implemented');
